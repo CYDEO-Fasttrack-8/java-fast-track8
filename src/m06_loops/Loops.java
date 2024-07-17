015c32ea62a3c33e32e70b12cfef941aba67e25f
Murodil <murodil@cydeo.com>
2024-07-16 20:04:53 -0400

    Day02 review code

--- src/m06_loops/Loops.java
+++ src/m06_loops/Loops.java
@@ -12,7 +12,16 @@ public class Loops {
         // can be replaced with:
 
         for(int i = 0; i < 5; i++){
-            System.out.println("Hello");
+            System.out.println(i + "Hello");
+        }
+
+        String str = "intellij";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(i + " -> " + str.charAt(i));
+        }
+                    //7                0     -1
+        for (int i = str.length()-1; i >= 0; i--) {
+            System.out.println(i + " -> " + str.charAt(i));
         }
 
         /*
@@ -35,9 +44,22 @@ public class Loops {
         }
         System.out.println(count);
 
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) == 'a') {
+//                count++;
+//            }
+//        }
+        System.out.println("count = " + count);
+
+        String str2 = "hello java";
+        int len1 = str2.length();
+        str2 = str2.replace("a","");
+        count = len1 - str2.length();
+        System.out.println("count = " + count);
+
         System.out.println("------------------------------------------------------");
 
-        String s2 = "abcdef";
+        String s2 = "abcdefz";
         int count2 = 0;
         do {
             count2++;
@@ -59,6 +81,22 @@ public class Loops {
                 System.out.println("i " + i + " j " + j);
             }
         }
+
+        String word1 = "abcdef";
+        String word2 = "ghabpoe";
+
+        //find common characters
+        //output = abe
+
+        for (int i = 0; i < word1.length(); i++) {
+            for (int j = 0; j < word2.length(); j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    System.out.println(word1.charAt(i));
+                }
+            }
+        }
+
+
 
     }
 }

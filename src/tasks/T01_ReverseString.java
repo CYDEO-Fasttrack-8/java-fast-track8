ff7758c60dc5595923ca793a2655eea5149e1ac8
Murodil <murodil@cydeo.com>
2024-07-16 20:28:58 -0400

    Day02 review code

---
+++ src/tasks/T01_ReverseString.java
@@ -0,0 +1,24 @@
+package tasks;
+
+public class T01_ReverseString {
+    public static void main(String[] args) {
+        String str = "hi all";
+
+        //1) using StringBuilder
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("reversed = " + reversed);
+        
+        //2) using a for loop
+        String rev = "";
+        for (int i = 0; i < str.length(); i++) {
+            rev = str.charAt(i) + rev;
+        }
+        System.out.println("rev = " + rev);
+        
+        String reversedStr = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+        System.out.println("reversedStr = " + reversedStr);
+    }
+}

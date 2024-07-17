299eda06852cbf75df255a4d20b76f43a32aad07
Murodil <murodil@cydeo.com>
2024-07-17 19:49:45 -0400

    Day03 review code

---
+++ src/m08_arrays/ForEachLoop.java
@@ -0,0 +1,23 @@
+package m08_arrays;
+
+import java.util.Arrays;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 12, 6};
+
+        //1) Using arrays.toString to print in same line as String
+        System.out.println(Arrays.toString(arr));
+
+        //2) for loop
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        
+        //3) for each
+        for(int n : arr) {
+            System.out.println("n = " + n);
+        }
+        
+    }
+}

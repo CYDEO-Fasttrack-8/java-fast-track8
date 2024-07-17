9e4a1b2e417fe0c8b8bbd07d28bc8d73636afbeb
Murodil <murodil@cydeo.com>
2024-07-17 19:05:55 -0400

    Day03 review code

--- src/m07_methods/MethodOverloading.java
+++ src/m07_methods/MethodOverloading.java
@@ -6,7 +6,16 @@ public class MethodOverloading {
         //Overloading - two or more methods with SAME name, and DIFFERENT parameters
         buy("townhome");
         buy((byte)2);
+
+        int n = 4;
+        buyByZipcode(n);
+        System.out.println("n after method = " + n);
     }
+
+    public static void main(int num) {
+
+    }
+
 
     // without any overloading:
 
@@ -20,10 +29,12 @@ public class MethodOverloading {
 
     public static void buyByZipcode(int zipcode) {
         System.out.println("Looking in zipcode " + zipcode);
+        zipcode = zipcode + 10;
     }
 
     public static void buyByRooms(byte rooms) {
         System.out.println("Looking " + rooms + " rooms");
+
     }
 
     // without overloading there is 4 separate method names that would need to be used and remembered
@@ -44,6 +55,11 @@ public class MethodOverloading {
 
     public static void buy(byte rooms) {
         System.out.println("Looking " + rooms + " rooms");
+    }
+
+    private String buy(boolean hasGarage) {
+        System.out.println("looking for garage - " + hasGarage);
+        return "garage homes";
     }
 
     // taking those same method but overloading them with the same name allows the above methods to be grouped better. There is still 4 methods that can do slightly different things, but they had a similar idea so overloading allowed them to share one name and different parameters

fe64b9b0a7e6fb50861f04637109685fe02a1fe5
Murodil <murodil@cydeo.com>
2024-07-17 20:06:15 -0400

    Day03 review code

--- src/tasks/T04_Fibonacci.java
+++ src/tasks/T04_Fibonacci.java
@@ -1,4 +1,7 @@
 package tasks;
+
+import java.util.Arrays;
+
 //0, 1, 1, 2, 3, 5, 8
 public class T04_Fibonacci {
     public static void main(String[] args) {
@@ -13,5 +16,16 @@ public class T04_Fibonacci {
             first = second;
             second = next;
         }
+
+        //2) second way using array
+        int[] fib = new int[7];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= 6; i++) {
+            fib[i] = fib[i-2] + fib[i-1];
+        }
+        System.out.println(Arrays.toString(fib));
+
     }
 }

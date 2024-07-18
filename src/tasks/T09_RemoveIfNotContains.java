e66834284a53a1afd079596af10dac5aa1fe65fa
Murodil <murodil@cydeo.com>
2024-07-18 19:47:22 -0400

    Day04 review code

---
+++ src/tasks/T09_RemoveIfNotContains.java
@@ -0,0 +1,25 @@
+package tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+The given strings  are: [rabbit   bribe   bike  ]
+The given word is: bib
+
+Filter all the words from list 1 which contains the given word.
+
+for eg : rabbit     has  b , i and b.
+where as bike only has b and i.
+
+So expected output :  rabbit   bribe
+ */
+public class T09_RemoveIfNotContains {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>(Arrays.asList("rabbit" ,  "bribe" ,  "bike"));
+        String word = "bib";
+
+
+    }
+}

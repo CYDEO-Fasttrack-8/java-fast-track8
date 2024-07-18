dfdfb8cc2098fe753785a0d34a5737f476b138e6
Murodil <murodil@cydeo.com>
2024-07-17 20:46:02 -0400

    Day03 review code

--- src/tasks/T06_Anagram.java
+++ src/tasks/T06_Anagram.java
@@ -3,8 +3,18 @@ package tasks;
 import java.util.Arrays;
 
 public class T06_Anagram {
-//'a','b','c',   'a','b','c'
+    public static void main(String[] args) {
+        System.out.println(isAnagram("abc","cab"));
+        System.out.println(isAnagram("listen", "silent"));
+        System.out.println(isAnagram("Dormitory", "Dirty Room"));
+    }
+
     public static boolean isAnagram(String s1, String s2) {
+
+        //remove spaces, and convert to lowercase
+        s1 = s1.replace(" ","").toLowerCase();
+        s2 = s2.replace(" ","").toLowerCase();
+
         if (s1.length() != s2.length()) return false;
 
         char[] arr1 = s1.toCharArray();

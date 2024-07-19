4ce2a42da576ec65878d5cec5191846bbf6313c6
Murodil <murodil@cydeo.com>
2024-07-18 20:16:59 -0400

    Day04 review code

--- src/tasks/T09_RemoveIfNotContains.java
+++ src/tasks/T09_RemoveIfNotContains.java
@@ -20,6 +20,35 @@ public class T09_RemoveIfNotContains {
         List<String> words = new ArrayList<>(Arrays.asList("rabbit" ,  "bribe" ,  "bike"));
         String word = "bib";
 
+        List<String> filteredWords = new ArrayList<>();
+
+        for(String each : words) {
+            //create array with each character in separate index
+            String[] arr = each.split("");
+            String[] arrTarget = word.split("");
+
+            boolean match = true;
+            for (String eachCh : arrTarget) {
+                //get count of char in word form list
+                long count = Arrays.stream(arr)
+                        .filter(x -> x.equals(eachCh))
+                        .count();
+                //get count of char in word form target word
+                long countFromWord = Arrays.stream(arrTarget)
+                        .filter(x -> x.equals(eachCh))
+                        .count();
+                //if they are not equal , fail it
+                if (count != countFromWord) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                filteredWords.add(each);
+            }
+        }
+
+        System.out.println("filteredWords = " + filteredWords);
 
     }
 }

ebcac5bb49135bb25c8bdab9ffea680cbf60297b
Murodil <murodil@cydeo.com>
2024-07-22 19:00:19 -0400

    Day05 review code

--- src/tasks/T11_TwoSumIndexes.java
+++ src/tasks/T11_TwoSumIndexes.java
@@ -1,11 +1,22 @@
 package tasks;
 
-public class T11_TwoSumIndexes {
-    public static void main(String[] args) {
-        /*
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
             array,           target -> need to return 2 indexes that sum to the target
             [3, 5, 1, 2, 9] -> 10 -> 2, 4
          */
+public class T11_TwoSumIndexes {
+    public static void main(String[] args) {
+        int[] nums1 = {3, 5, 1, 2, 9};
+        int[] indexes = find2Sum(nums1, 10);
+        System.out.println(Arrays.toString(indexes));
+
+        int[] result = findTwoSum(nums1, 10);
+        System.out.println(Arrays.toString(result));
+
     }
     //nested loops
     public static int[] find2Sum(int[] nums, int target) {
@@ -17,6 +28,20 @@ public class T11_TwoSumIndexes {
             }
         }
         return new int[2];//0,0
+    }
+        //[3, 5, 1, 2, 9] -> 10
+    public static int[] findTwoSum(int[] nums, int target) {
+        // arr value, index
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff) , i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[2];
     }
 
 }

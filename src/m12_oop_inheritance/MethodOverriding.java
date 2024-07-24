fce21e15c0c98fa238e18c41e61ce7bbb070945e
Murodil <murodil@cydeo.com>
2024-07-23 20:40:22 -0400

    Day6 review code

--- src/m12_oop_inheritance/MethodOverriding.java
+++ src/m12_oop_inheritance/MethodOverriding.java
@@ -1,28 +1,30 @@
 package m12_oop_inheritance;
 
 public class MethodOverriding {
+}
 
-    class Sport {
-        void perform() {
-            System.out.println("Performing sport");
-        }
-
-        @Override
-        public String toString() {
-            return "Sports " + super.getClass().getSimpleName();
-        }
+class Sport {
+    void perform() {
+        System.out.println("Performing sport");
     }
 
-    class Golf extends Sport{
-        @Override
-        void perform() {
-            System.out.println("Swinging club");
-        }
-    }
-    class Soccer extends Sport{
-        @Override
-        void perform() {
-            System.out.println("Kicking ball");
-        }
+    @Override
+    public String toString() {
+        return "Sports " + super.getClass().getSimpleName();
     }
 }
+
+class Golf extends Sport{
+    @Override
+    void perform() {
+        System.out.println("Swinging club");
+    }
+}
+
+class Soccer extends Sport{
+    @Override
+    void perform() {
+        System.out.println("Kicking ball");
+    }
+}
+

601b6da419669d322e1a1be3fc88ae7ff7805675
Murodil <murodil@cydeo.com>
2024-07-23 20:57:46 -0400

    Day6 review code

--- src/m12_oop_inheritance/MethodOverriding.java
+++ src/m12_oop_inheritance/MethodOverriding.java
@@ -1,11 +1,29 @@
 package m12_oop_inheritance;
 
 public class MethodOverriding {
+    public static void main(String[] args) {
+        new Sport().perform();
+        new Golf().perform();
+        new Soccer().perform();
+        System.out.println(new Soccer());
+        System.out.println(new Golf());
+
+        System.out.println("------------------------------");
+        Building.open();
+        Shed.open();
+
+        System.out.println(Building.a);
+        System.out.println(Shed.a);
+    }
 }
 
-class Sport {
+class Sport extends Object{
     void perform() {
         System.out.println("Performing sport");
+    }
+
+    public void perform(String action) {
+        System.out.println("Performing " + action);
     }
 
     @Override
@@ -16,15 +34,30 @@ class Sport {
 
 class Golf extends Sport{
     @Override
-    void perform() {
+    protected void perform() {
         System.out.println("Swinging club");
     }
 }
 
 class Soccer extends Sport{
     @Override
-    void perform() {
+    public void perform() {
         System.out.println("Kicking ball");
     }
 }
 
+
+// static methods cannot be overridden. static method and variables are HIDDEN
+class Building {
+    public static int a = 10;
+    public static void open() {
+        System.out.println("Opening Building");
+    }
+}
+class Shed extends Building{
+    public static int a = 30;
+
+    public static void open() {
+        System.out.println("Opening Shed");
+    }
+}

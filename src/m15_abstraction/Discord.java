e965aaf6e7cea6c0d87799f050e3304557988fc8
Murodil <murodil@cydeo.com>
2024-07-24 19:56:16 -0400

    Day7 review code

---
+++ src/m15_abstraction/Discord.java
@@ -0,0 +1,11 @@
+package m15_abstraction;
+/*
+Discord is CONCRETE class since it is extending
+abstract class. and must override abstract methods
+ */
+public class Discord extends App {
+    @Override
+    public void launch() {
+        System.out.println("Launching discord server..");
+    }
+}

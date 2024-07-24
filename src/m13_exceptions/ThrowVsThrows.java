4da9208bc19a4958c9f264bb8e2f4b8804e3e137
Murodil <murodil@cydeo.com>
2024-07-24 19:04:46 -0400

    Day7 review code

--- src/m13_exceptions/ThrowVsThrows.java
+++ src/m13_exceptions/ThrowVsThrows.java
@@ -1,11 +1,19 @@
 package m13_exceptions;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
+/*
+Throws keyword is used to DECLARE that method might throw exception
+In checked exceptions, it will help our code to compile, without using try/catch
+ */
 public class ThrowVsThrows {
+    public static void main(String[] args) throws IOException {
+        String data = readFile("src/m13_exceptions/data.txt");
+        System.out.println("data = " + data);
+    }
 
-    public static String readFile(String filePath) {
+    public static String readFile(String filePath) throws IOException{ //DECLARE this method throws an Exception
         return Files.readAllLines(Path.of(filePath)).toString();
     }
 

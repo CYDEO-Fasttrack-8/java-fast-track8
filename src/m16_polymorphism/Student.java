ca901632163f1d0518c96179b807871ab1acb243
Murodil <murodil@cydeo.com>
2024-07-24 20:51:29 -0400

    Day7 review code

---
+++ src/m16_polymorphism/Student.java
@@ -0,0 +1,13 @@
+package m16_polymorphism;
+
+public class Student extends Person {
+    private String school;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+}

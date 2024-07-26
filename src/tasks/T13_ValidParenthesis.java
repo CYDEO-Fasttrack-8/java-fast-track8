46c885139ef639e71a5417693266f9f531ef289e
Murodil <murodil@cydeo.com>
2024-07-25 21:22:34 -0400

    Day8 review code

--- src/tasks/T13_ValidParenthesis.java
+++ src/tasks/T13_ValidParenthesis.java
@@ -7,19 +7,30 @@ import java.util.Stack;
 //{([])} // []() // {} // {([{])}
 public class T13_ValidParenthesis {
     public static void main(String[] args) {
+        System.out.println(isValid("[]()"));
+        System.out.println(isValid("[)"));
+    }
+    public static boolean isValid(String str) {
         //use map to hold valid pairs
-        Map<String, String> map = new HashMap<>();
-        map.put("(", ")");
-        map.put("{", "}");
-        map.put("[", "]");
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
 
         Stack<Character> stack = new Stack<>();
 
-        String str = "[]()";
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
             //check if currentChar is opening bracket
-
+            if (map.containsKey(currentChar)) {
+                stack.push(currentChar); //{([
+            } else {//closing bracket      )               ]      [
+                if (stack.isEmpty() || currentChar != map.get(stack.pop())) {
+                    return false;
+                }
+            }
         }
+
+        return stack.isEmpty();
     }
 }
